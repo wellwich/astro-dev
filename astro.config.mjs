d24ef3ecd652957d04320585d906f7c6d8966284
wellwich <mail@wellwich.com>
2024-06-13 08:04:10 +0900

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "static",
+  output: "hybrid",
   adapter: cloudflare({
     platformProxy: {
       enabled: true
